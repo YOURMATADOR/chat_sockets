e80113678eb5d90e7b3e317a1039849ae81a202a
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2019-02-27 22:12:59 -0600

    conexion cliente-servidor leer escribir :metal:

---
+++ src/sistemas_distribuidos/Cliente.java
@@ -0,0 +1,99 @@
+package sistemas_distribuidos;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Cliente {
+
+    private final int PUERTO = 1234; //Puerto para la conexi�n
+    private final String HOST = "localhost"; //Host para la conexi�n
+    private String mensajeServidor; //Mensajes entrantes (recibidos) en el servidor
+    cl_interfaz interfaz;
+
+    public String getMensajeServidor() {
+        return mensajeServidor;
+    }
+
+    public void setMensajeServidor(String mensajeServidor) {
+        this.mensajeServidor = mensajeServidor;
+    }
+
+    public void set_interfaz(cl_interfaz cl) {
+        this.interfaz = cl;
+    }
+
+    public Socket getCs() {
+        return cs;
+    }
+
+    public void setCs(Socket cs) {
+        this.cs = cs;
+    }
+
+    public BufferedWriter getSalidaServidor() {
+        return salidaServidor;
+    }
+
+    public void setSalidaServidor(BufferedWriter salidaServidor) {
+        this.salidaServidor = salidaServidor;
+    }
+
+    public DataOutputStream getdOut() {
+        return dOut;
+    }
+
+    public void setdOut(DataOutputStream dOut) {
+        this.dOut = dOut;
+    }
+    private Socket cs; //Socket del cliente
+    private BufferedWriter salidaServidor; //Flujo de datos de salida
+    DataOutputStream dOut;
+
+    public Cliente() throws IOException {
+        cs = new Socket(HOST, PUERTO); //Socket para el cliente en localhost en puerto 1234
+        System.out.println("Iniciando cliente\n");
+        dOut = new DataOutputStream(cs.getOutputStream());
+    }
+    public void setConversasion(String conversasion) {
+        System.out.println(conversasion);
+        this.interfaz.actualiza_conversasion(conversasion);
+
+    }
+    public void enviar_mensaje(String mensaje) throws IOException {
+
+        OutputStream out;
+
+        try {
+            out = cs.getOutputStream();
+            //Flujo de datos hacia el servidor 
+            salidaServidor = new BufferedWriter(new OutputStreamWriter(out));
+            salidaServidor.write(mensaje);
+            salidaServidor.newLine();
+            salidaServidor.flush();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void startClient() { //M�todo para iniciar el cliente
+        String message;
+
+        while (true) {
+            //If there are new messages
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader((cs.getInputStream())));
+                if ((message = br.readLine()) != null) {
+                    //Show them
+                     this.setConversasion("Servidor: " + message);
+                    System.out.println(message);
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/sistemas_distribuidos/Servidor.java
@@ -0,0 +1,78 @@
+package sistemas_distribuidos;
+
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+
+    private final int PUERTO = 1234; //Puerto para la conexi�n
+    private final String HOST = "localhost"; //Host para la conexi�n
+    private String mensajeServidor; //Mensajes entrantes (recibidos) en el servidor    
+    sv_interfaz interfaz = null;
+
+    public void setConversasion(String conversasion) {
+        System.out.println(conversasion);
+        this.interfaz.actualiza_conversasion(conversasion);
+
+    }
+
+    private ServerSocket ss; //Socket del servidor
+    private Socket cs; //Socket del cliente
+
+    public void set_interfaz(sv_interfaz inter) {
+        this.interfaz = inter;
+    }
+
+    public void enviar_mensaje(String mensaje) throws IOException {
+        BufferedWriter salidaServidor; //Flujo de datos de salida
+
+        OutputStream out;
+
+        try {
+            out = cs.getOutputStream();
+            //Flujo de datos hacia el servidor 
+            salidaServidor = new BufferedWriter(new OutputStreamWriter(out));
+            salidaServidor.write(mensaje);
+            salidaServidor.newLine();
+            salidaServidor.flush();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Servidor() throws IOException {
+        ss = new ServerSocket(PUERTO);//Se crea el socket para el servidor en puerto 1234
+        cs = new Socket(); //Socket para el cliente
+
+        System.out.println("Iniciando servidor\n");
+
+    }
+
+  
+
+    public void startServer() {   //M�todo para iniciar el servidor   
+        try {
+            System.out.println("Esperando..."); //Esperando conexi�n
+
+            cs = ss.accept(); //Accept comienza el socket y espera una conexi�n desde un cliente
+            System.out.println("Cliente en l�nea");
+        this.setConversasion("Cliente conectado!");
+            //Se obtiene el flujo entrante desde el cliente
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+
+            while ((mensajeServidor = entrada.readLine()) != null) { //Mientras haya mensajes desde el cliente            
+                //Se muestra por pantalla el mensaje recibido
+                this.setConversasion("Cliente: " + mensajeServidor);
+                System.out.println(mensajeServidor);
+            }
+
+            System.out.println("Fin de la conexi�n");
+
+            ss.close();//Se finaliza la conexi�n con el cliente
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
